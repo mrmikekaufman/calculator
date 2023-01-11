42f45d569cf946a78b7a844e9bf99d1d712c2f20
Michael Kaufman <mrmikekaufman@gmail.com>
2023-01-11 12:13:14 -0800

    Update display with button press
    Pressing buttons will update display. Numbers are stored before
    and after operator is pressed. Number one and operator are displayed
    on top of display, number two is displayed on bottom.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,30 @@
-const operators = document.querySelectorAll('.op');
+const numbers = document.querySelectorAll('.num');
+const math = document.querySelectorAll('.math');
+const topDisplay = document.querySelector('.display-top');
+const bottomDisplay = document.querySelector('.display-bottom');
+let num1 = '';
+let num2 = '';
+let operatorValue = '';
 
-operators.forEach((operator) => {
-    operator.addEventListener('click', () => {
-        alert(operator.textContent);
+numbers.forEach((num) => {
+    num.addEventListener('click', () => {
+        if (operatorValue === '') {
+            num1 += num.textContent;
+            topDisplay.textContent = `${num1}`;
+        } else {
+            num2 += num.textContent;
+            bottomDisplay.textContent = `${num2}`;
+        }
     })
 })
+
+math.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        operatorValue = operator.textContent;
+        topDisplay.textContent = `${num1} ${operatorValue}`;
+    })
+})
+
 
 function add(num1, num2) {
     return num1 + num2;
