b10d48482d8683bcbc53145e306b1834809610ba
Michael Kaufman <mrmikekaufman@gmail.com>
2023-01-11 10:37:38 -0800

    Create functions for all operators
    Functions created in javascript for all potential operations.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,34 @@ const operators = document.querySelectorAll('.op');
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        alert(operator.textContent * 2);
+        alert(operator.textContent);
     })
 })
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    if(operator === '/') {
+        divide(num1, num2);
+    } else if(operator === '+') {
+        add(num1, num2);
+    } else if(operator === '-') {
+        subtract(num1, num2);
+    } else if(operator === '*') {
+        multiply(num1, num2);
+    }
+}
